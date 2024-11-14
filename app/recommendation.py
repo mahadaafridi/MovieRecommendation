88a076a78e25ee1f0ef538e65b5441a388f5baa7
Mahad Afridi <mahadakbarafridi@gmail.com>
2024-11-13 21:24:21 -0800

    added similarity scores and images

--- app/recommendation.py
+++ app/recommendation.py
@@ -7,19 +7,21 @@ import os
 
 load_dotenv()
 
+#env variable
 API_KEY = os.getenv("API_KEY")
 
-def get_poster_url(movie_title):
+def get_url(movie_title: str):
     url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={movie_title}"
     response = requests.get(url)
     data = response.json()
     
-    if data['results']:
-        poster_path = data['results'][0].get('poster_path')
-        return f"https://image.tmdb.org/t/p/w500{poster_path}" if poster_path else None
-    return None
-
-def get_recommendations(movie_name):
+    poster_path = data['results'][0].get('poster_path')
+    if poster_path:
+        return f"https://image.tmdb.org/t/p/w500{poster_path}"
+    else:
+        return None
+    
+def get_recommendations(movie_name: str):
     df = pd.read_csv("data/movie_dataset.csv")
     features = ['keywords', 'cast', 'genres', 'director']
     for feature in features:
@@ -35,12 +37,16 @@ def get_recommendations(movie_name):
     similar_movies = list(enumerate(cosine_sim[movie_index]))
     sorted_similar_movies = sorted(similar_movies, key=lambda x: x[1], reverse=True)
 
+    #contiains the titles, poster urls, and similarity scores of all the movies
     recommendations = []
 
     #skip over the first rec because it will be the exact same as the one entered
-    for i in range(1, 10):
+    #return only top 10 so it doesn't overwhelm the user
+    for i in range(1, 11):
         title = df.iloc[sorted_similar_movies[i][0]]['title']
-        poster_url = get_poster_url(title)
-        recommendations.append({"title": title, "poster_url": poster_url})
+        poster_url = get_url(title)
+        #add similarity score 
+        similarity_score = round(sorted_similar_movies[i][1] * 100)
+        recommendations.append({"title": title, "poster_url": poster_url, "similarity_score": similarity_score})
     
     return recommendations
--- run.py
+++ run.py
@@ -3,4 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
+    #run on port 5001 since I have MySQL on port 5000
     app.run(debug=True, host="0.0.0.0", port=5001)
